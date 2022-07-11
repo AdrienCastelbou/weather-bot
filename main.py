f75909cc134376201428a9190cd3a94d8811ff87
Adrien Castelbou <adriencastelbou@MacBook-Air-de-Adrien.local>
2022-07-11 18:16:55 +0200

    get weather by hour working

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from google.api_core.exceptions import InvalidArgument
 import requests
 import json
 from geopy.geocoders import Nominatim
+import dateutil.parser
 
 load_dotenv()
 # Dialogflow env variables
@@ -15,14 +16,14 @@ DIALOGFLOW_PROJECT_ID = os.getenv('DIALOGFLOW_PROJECT_ID')
 DIALOGFLOW_LANGUAGE_CODE = os.getenv('DIALOGFLOW_LANGUAGE_CODE')
 SESSION_ID = os.getenv('SESSION_ID')
 
-# OpenWeatherMap env variables
+# OpenWeatherMap env variable
 OPEN_WEATHER_API_KEY = os.getenv('OPEN_WEATHER_API_KEY')
 
 
-def get_intent():
+def get_intent(query):
     session_client = dialogflow.SessionsClient()
     session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
-    text_input = dialogflow.types.TextInput(text= sys.argv[1], language_code=DIALOGFLOW_LANGUAGE_CODE)
+    text_input = dialogflow.types.TextInput(text= query, language_code=DIALOGFLOW_LANGUAGE_CODE)
     query_input = dialogflow.types.QueryInput(text=text_input)
     try:
         response = session_client.detect_intent(session=session, query_input=query_input)
@@ -58,21 +59,38 @@ def get_date(response):
     return date
 
 
+def get_weather_by_hour(wanted_date, report):
+    wanted_date = dateutil.parser.parse(wanted_date)
+    dt = wanted_date.timestamp()
+    hourly_report = report["hourly"]
+    for entry in hourly_report:
+        if entry["dt"] >= dt:
+            return entry
+
+
 def get_weather(location, date):
     open_weather_url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % \
                        (location.latitude, location.longitude, OPEN_WEATHER_API_KEY)
     response = requests.get(open_weather_url)
-    data = json.loads(response.text)
-    print(date, " we will have ", data["current"]["weather"][0]["description"], ", with ", data["current"]["temp"],
-          " degrees")
+    report = json.loads(response.text)
+    return report
 
 
 def main():
-    response = get_intent()
+    if len(sys.argv) != 2:
+        print('Bad number of arguments')
+        return 1
+    query = sys.argv[1]
+    response = get_intent(query)
     location = get_location(response)
     date = get_date(response)
-    print(location)
-    get_weather(location, date)
+    report = get_weather(location, date)
+    hourly_report = get_weather_by_hour(date, report)
+    date = dateutil.parser.parse(date)
+    print(date.strftime("%A %d %B %H %M"), "we will have", hourly_report["weather"][0]["description"], ", with ",
+          hourly_report["temp"],
+          " degrees")
+    return 0
 
 
 if __name__ == "__main__":

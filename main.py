a7ac62dcf397d41f9276a7d5e9dd410596149147
Adrien Castelbou <adriencastelbou@MacBook-Air-de-Adrien.local>
2022-07-15 11:33:56 +0200

    add tcp server

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
 import os
-import sys
+import socket
 import dialogflow
 from dotenv import load_dotenv
 from google.api_core.exceptions import InvalidArgument
@@ -23,29 +24,19 @@ OPEN_WEATHER_API_KEY = os.getenv('OPEN_WEATHER_API_KEY')
 def get_intent(query):
     session_client = dialogflow.SessionsClient()
     session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
-    text_input = dialogflow.types.TextInput(text= query, language_code=DIALOGFLOW_LANGUAGE_CODE)
+    text_input = dialogflow.types.TextInput(text=query, language_code=DIALOGFLOW_LANGUAGE_CODE)
     query_input = dialogflow.types.QueryInput(text=text_input)
     try:
-        response = session_client.detect_intent(session=session, query_input=query_input)
-        if response.query_result.intent.display_name != "get-weather" or \
-                response.query_result.intent_detection_confidence < 0.7:
-            print("Do you want the weather ?")
-            exit()
+        intent = session_client.detect_intent(session=session, query_input=query_input)
     except InvalidArgument:
-        print("bouhou")
         raise
-    return response
+    return intent
 
 
 def get_location(response):
     geolocator = Nominatim(user_agent="weather-bot")
     items = response.query_result.parameters.items()
-
-    date = next(item for item in items if item[0] == "date-time")[1]
     city = next(item for item in items if item[0] == "geo-city")[1]
-    if not isinstance(date, str):
-        date_item = date.items()
-        date = date_item[0][1]
     location = geolocator.geocode(city)
     return location
 
@@ -53,7 +44,9 @@ def get_location(response):
 def get_date(response):
     items = response.query_result.parameters.items()
     date = next(item for item in items if item[0] == "date-time")[1]
-    if not isinstance(date, str):
+    if not date:
+        date = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S+02:00")
+    elif not isinstance(date, str):
         date_item = date.items()
         date = date_item[0][1]
     return date
@@ -76,21 +69,57 @@ def get_weather(location, date):
     return report
 
 
-def main():
-    if len(sys.argv) != 2:
-        print('Bad number of arguments')
-        return 1
-    query = sys.argv[1]
-    response = get_intent(query)
-    location = get_location(response)
-    date = get_date(response)
+def process_weather_request(intent):
+    location = get_location(intent)
+    date = get_date(intent)
+    print("date = ", date, "\n")
     report = get_weather(location, date)
     hourly_report = get_weather_by_hour(date, report)
     date = dateutil.parser.parse(date)
-    print(date.strftime("%A %d %B %H %M"), "we will have", hourly_report["weather"][0]["description"], ", with ",
-          hourly_report["temp"],
-          " degrees")
-    return 0
+    return date.strftime("%A %d %B %H %M") + ", we will have " + hourly_report["weather"][0]["description"] + \
+        ", with " + str(hourly_report["temp"]) + " degrees\n"
+
+
+def process_query(query):
+    intent = get_intent(query)
+    if intent.query_result.intent.display_name == "get-weather" and \
+            intent.query_result.intent_detection_confidence >= 0.7:
+        response = process_weather_request(intent)
+    else:
+        response = "Bro what do you mean ?\n"
+    return response
+
+
+def init_tcp_server():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ('localhost', 4242)
+    sock.bind(server_address)
+    sock.listen(1)
+    while True:
+        print("Waiting for socket connection...")
+        try:
+            conn, addr = sock.accept()
+        except KeyboardInterrupt:
+            break
+        try:
+            print("New connection")
+            while True:
+                print("waiting for question...")
+                data = conn.recv(1024)
+                if not data:
+                    break
+                response = process_query(query=data.decode())
+                conn.sendall(response.encode())
+        except KeyboardInterrupt:
+            conn.close()
+            break
+        finally:
+            print("close")
+            conn.close()
+
+
+def main():
+    init_tcp_server()
 
 
 if __name__ == "__main__":
